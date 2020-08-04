d79cd694c71819cce131831087d63fbe4bb2c6d5
zjun <565061763@qq.com>
2020-08-04 15:01:07 +0800

    BaseFootPanel

--- lib/src/main/java/com/sd/lib/bodyscroller/panel/KeyboardFootPanel.java
+++ lib/src/main/java/com/sd/lib/bodyscroller/panel/KeyboardFootPanel.java
@@ -4,54 +4,51 @@ import android.app.Activity;
 
 import com.sd.lib.bodyscroller.ext.FKeyboardListener;
 
-public class KeyboardFootPanel implements IFootPanel
+public class KeyboardFootPanel extends BaseFootPanel
 {
     private final FKeyboardListener mKeyboardListener;
-    private IFootPanel.HeightChangeCallback mCallback;
-    private int mHeight;
 
     public KeyboardFootPanel(Activity activity)
     {
         mKeyboardListener = FKeyboardListener.of(activity);
     }
 
+    /**
+     * 监听软键盘
+     */
     private final FKeyboardListener.Callback mKeyboardCallback = new FKeyboardListener.Callback()
     {
         @Override
         public void onKeyboardHeightChanged(int oldHeight, int newHeight, FKeyboardListener listener)
         {
-            KeyboardFootPanel.this.setHeight(newHeight);
+            final HeightChangeCallback callback = getHeightChangeCallback();
+            if (callback != null)
+            {
+                callback.onHeightChanged(newHeight);
+            } else
+            {
+                releasePanel();
+            }
         }
     };
-
-    private void setHeight(int height)
-    {
-        if (mHeight != height)
-        {
-            mHeight = height;
-
-            if (mCallback != null)
-                mCallback.onHeightChanged(height);
-        }
-    }
 
     @Override
     public int getPanelHeight()
     {
-        return mHeight;
+        return mKeyboardListener.getKeyboardHeight();
     }
 
     @Override
     public void initPanel(HeightChangeCallback callback)
     {
-        mCallback = callback;
+        super.initPanel(callback);
         mKeyboardListener.addCallback(mKeyboardCallback);
     }
 
     @Override
     public void releasePanel()
     {
-        mCallback = null;
+        super.releasePanel();
         mKeyboardListener.removeCallback(mKeyboardCallback);
     }
 }
